f1b4183a4ab1324b782e6cac7ac612249a9259f8
Sela <vondeesela@gmail.com>
2021-11-19 17:44:10 +0000

    Updates

---
+++ precache-manifest.be388816869987e4cfd997c875783e6f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e609abb3786bf76c422814d58d6f7aef",
+    "url": "/blockwatch_graph/index.html"
+  },
+  {
+    "revision": "51f55291176a3488fe44",
+    "url": "/blockwatch_graph/static/js/2.a66a7cab.chunk.js"
+  },
+  {
+    "revision": "a0a07717e68f9de0acc3",
+    "url": "/blockwatch_graph/static/js/main.00b7b807.chunk.js"
+  },
+  {
+    "revision": "2bce64983b5d649f228b",
+    "url": "/blockwatch_graph/static/js/runtime-main.bd0dc346.js"
+  }
+]);
